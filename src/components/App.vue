5de973d2698e25eaa32abec59677d4340b92694e
Inggo E. <inggo.espinosa@gmail.com>
2026-04-24 17:45:25 +0800

    Fix Firefox print layout

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,55 +4,51 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
 import Cube from './InggoCube.vue';
 
-export default {
-  name: 'App',
-  components: { Cube },
-  data() {
-    return { loaded: false };
-  },
-  mounted() {
-    fetch('/data.json')
-      .then((r) => r.json())
-      .then((data) => this.populateContents(data))
-      .catch((err) => console.error(err));
-  },
-  methods: {
-    populateContents(data) {
-      const store = this.$store;
-      store.commit('setFace', {
-        face: 'front', name: 'Bio', icon: 'icon-bio',
-        content: data.info, 'content-type': 'bio',
-      });
-      store.commit('setFace', {
-        face: 'right', name: 'Portfolio', icon: 'icon-folder',
-        content: data.portfolio, 'content-type': 'portfolio',
-        showZoom: true, cover: true,
-      });
-      store.commit('setFace', {
-        face: 'left', name: 'Work Experience', icon: 'icon-briefcase',
-        content: data.work_experience, 'content-type': 'work',
-        showZoom: true, cover: true,
-      });
-      store.commit('setFace', {
-        face: 'top', name: 'Education', icon: 'icon-education',
-        content: data.education, 'content-type': 'education',
-        showZoom: true, cover: true,
-      });
-      store.commit('setFace', {
-        face: 'bottom', name: 'Skills', icon: 'icon-tools',
-        content: data.skills, 'content-type': 'skills',
-        showZoom: true, cover: true,
-      });
-      store.commit('setFace', {
-        face: 'back', name: 'Links', icon: 'icon-link',
-        content: data.info, 'content-type': 'links',
-        showZoom: false,
-      });
-      this.loaded = true;
-    },
-  },
-};
+const loaded = ref(false);
+const store = useStore();
+
+onMounted(() => {
+  fetch('/data.json')
+    .then((r) => r.json())
+    .then((data) => populateContents(data))
+    .catch((err) => console.error(err));
+});
+
+function populateContents(data) {
+  store.commit('setFace', {
+    face: 'front', name: 'Bio', icon: 'icon-bio',
+    content: data.info, 'content-type': 'bio',
+  });
+  store.commit('setFace', {
+    face: 'right', name: 'Portfolio', icon: 'icon-folder',
+    content: data.portfolio, 'content-type': 'portfolio',
+    showZoom: true, cover: true,
+  });
+  store.commit('setFace', {
+    face: 'left', name: 'Work Experience', icon: 'icon-briefcase',
+    content: data.work_experience, 'content-type': 'work',
+    showZoom: true, cover: true,
+  });
+  store.commit('setFace', {
+    face: 'top', name: 'Education', icon: 'icon-education',
+    content: data.education, 'content-type': 'education',
+    showZoom: true, cover: true,
+  });
+  store.commit('setFace', {
+    face: 'bottom', name: 'Skills', icon: 'icon-tools',
+    content: data.skills, 'content-type': 'skills',
+    showZoom: true, cover: true,
+  });
+  store.commit('setFace', {
+    face: 'back', name: 'Links', icon: 'icon-link',
+    content: data.info, 'content-type': 'links',
+    showZoom: false,
+  });
+  loaded.value = true;
+}
 </script>
